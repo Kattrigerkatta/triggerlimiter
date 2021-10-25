eb773048d17eb491445190cc4f79fb006c4e2487
Kattrigerkatta <66407275+Kattrigerkatta@users.noreply.github.com>
2021-10-25 17:03:41 +0200

    file

---
+++ server.lua
@@ -0,0 +1,28 @@
+Config = {}
+Config.BlEvents = {
+  {name="esx_bank:redeem", limit=5, clrepeat=1} --clrepeat = min = what's the limit in clrepeat minutes, how often the event should get triggered, per player.
+}
+Citizen.CreatThread(function()
+  local hits = {}
+  for k, v in ipairs(Config.BlEvents) do
+    hits[v.name] = {}
+    AddEventHandler(v.name, function()
+      hits[v.name][source] = {}
+      if hits[v.name][source].hits == nil then
+        hits[v.name][source].hits = 1
+      else
+        hits[v.name][source].hits = hits[v.name].hits + 1
+      end
+      
+      if hits[v.name][source].hits > v.limit then -- The number of times you can trigger the event before the player is banned. IMPORTANT: You have to put your ban trigger in the empty column, because I don't provide a ban system.
+        -- ban-shit
+      end
+    end)
+    
+    while true do
+       Citizen.Wait(v.clrepeat*60000)
+       hits[v.name][source].hits = 0
+    end
+  end
+
+end)
