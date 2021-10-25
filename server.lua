b6e6b6af98c07a3e1ae73558bfd9e6aae98c281a
Kattrigerkatta <66407275+Kattrigerkatta@users.noreply.github.com>
2021-10-25 21:56:38 +0200

    fixed bug

--- server.lua
+++ server.lua
@@ -2,28 +2,38 @@ Config = {}
 Config.BlEvents = {
   {name="esx_bank:redeem", limit=5, clrepeat=1} --clrepeat = min = what's the limit in clrepeat minutes, how often the event should get triggered, per player.
 }
-Citizen.CreatThread(function()
-  local hits = {}
-  for k, v in ipairs(Config.BlEvents) do
-    hits[v.name] = {}
-    AddEventHandler(v.name, function()
-      hits[v.name][source] = {}
-      if hits[v.name][source].hits == nil then
-        hits[v.name][source].hits = 1
-      else
-        hits[v.name][source].hits = hits[v.name].hits + 1
-      end
-      
-      if hits[v.name][source].hits > v.limit then -- The number of times you can trigger the event before the player is banned. IMPORTANT: You have to put your ban trigger in the empty column, because I don't provide a ban system.
-        -- ban-shit
-        print('The player: ' .. GetPlayerName(source) .. ' triggered the event ' .. v.name ' more than ' .. v.limit .. ' times') -- EXAMPLE
-      end
-    end)
-    
-    while true do
-       Citizen.Wait(v.clrepeat*60000)
-       hits[v.name][source].hits = 0
-    end
-  end
+local hits = {}
 
+Citizen.CreateThread(function()
+    for k, v in ipairs(Config.BlEvents) do
+        hits[v.name] = {}
+        RegisterServerEvent(v.name)
+        AddEventHandler(v.name, function()
+            if hits[v.name][source] == nil then
+                hits[v.name][source] = {}
+                hits[v.name][source].hits = 1
+            else
+                hits[v.name][source].hits = hits[v.name][source].hits + 1
+            end
+            
+            if not hits[v.name][source].started then
+                hits[v.name][source].started = true
+                startTimer(v.name, source, v.clrepeat)
+            end
+
+            if hits[v.name][source].hits > v.limit then
+                print("" ..GetPlayerName(source).. " triggerte zu oft das Event: " ..v.name)
+                --Ban Event
+            end
+        end)
+    end
 end)
+
+function startTimer(name, player, rp)
+    Citizen.CreateThread(function()
+        while true do
+            Citizen.Wait(rp*60000)
+            hits[name][player].hits = 0
+        end
+    end)
+end
